93df5f0d5c5d7e231c3c4110686014de59548827
edosatk <gendabusen@gmail.com>
2024-06-19 11:30:14 +0000

    corrected

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -15,8 +15,8 @@ function countStudents(path) {
     const Final = {};
     fields.forEach((data) => { (Final[data] = 0); });
     newis.forEach((data) => { (Final[data[1]] += 1); });
-    console.log(`number of students: ${result.filter((check) => check.length > 3).length}`);
-    Object.keys(Final).forEach((data) => console.log(`number of students in ${data}: ${Final[data]}. List: ${newis.filter((n) => n[1] === data).map((n) => n[0]).join(', ')}`));
+    console.log(`Number of students: ${result.filter((check) => check.length > 3).length}`);
+    Object.keys(Final).forEach((data) => console.log(`Number of students in ${data}: ${Final[data]}. List: ${newis.filter((n) => n[1] === data).map((n) => n[0]).join(', ')}`));
   } catch (E) {
     throw Error('Cannot load the database');
   }
