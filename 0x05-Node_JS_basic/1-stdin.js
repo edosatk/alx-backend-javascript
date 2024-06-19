3c13ecb56ff41c8b039586726adf283bf61139ea
edosatk <gendabusen@gmail.com>
2024-06-19 12:00:47 +0000

    corrected

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ process.stdin.resume();
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', function(){
-  const chunk = process.stdin.read();
+  let chunk = process.stdin.read();
   if (chunk !== null) {
     process.stdout.write(`Your name is: ${chunk}`);
   }
