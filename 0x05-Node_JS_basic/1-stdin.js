3d59d48916c872099e21a394ed5442e4bf8cda4c
edosatk <gendabusen@gmail.com>
2024-11-29 17:31:56 +0000

    corrected and commited all in one

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,15 @@
 process.stdin.setEncoding('utf8');
 
 process.stdout.write('Welcome to Holberton School, what is your name?'+'\n');
+process.stdin.resume();
 process.stdin.on('readable', function(){
+	process.stdin.resume()
 	let chunk = process.stdin.read();
 	if(chunk){
 		process.stdout.write('Your name is: ' + chunk);
 	}
 });
+process.stdin.resume();
 process.stdin.on('end', function(){
 	process.stdout.write('This important software is now closing'+'\n');
 });
